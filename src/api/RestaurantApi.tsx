29192f049fad555e0230cc7ab55f13b19709f2df
RAITILAK <raitilak1212@gmail.com>
2025-02-14 00:02:48 +0530

    added search page and filter

---
+++ src/api/RestaurantApi.tsx
@@ -0,0 +1,54 @@
+import { SearchState } from "@/pages/SearchPage";
+import { RestaurantSearchResponse } from "@/types";
+import { useQuery } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useSearchRestaurants = (
+  searchState: SearchState,
+  city?: string
+) => {
+  const createSearchRequest = async (
+    searchState: SearchState
+  ): Promise<RestaurantSearchResponse> => {
+    const params = new URLSearchParams();
+    params.set("searchQuery", searchState.searchQuery);
+    params.set("page", searchState.page.toString());
+    params.set("selectedCuisines", searchState.selectedCuisines.join(","));
+    params.set("sortOption", searchState.sortOption);
+
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant");
+    }
+    return response.json();
+  };
+
+  // const { data: results, isLoading } = useQuery(
+  //   ["searchRestaurants", searchState.searchQuery, city,],
+  //   createSearchRequest,
+  //   {
+  //     enabled: !!city,
+  //   }
+  // );
+  const { data: results, isLoading } = useQuery(
+    [
+      "searchRestaurants",
+      searchState.searchQuery,
+      city,
+      searchState.page,
+      searchState.selectedCuisines,
+      searchState.sortOption,
+    ], // ✅ Now listens for page changes
+    () => createSearchRequest(searchState), // ✅ Pass searchState dynamically
+    { enabled: !!city }
+  );
+
+  return {
+    results,
+    isLoading,
+  };
+};
---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,192 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
+import { useParams } from "react-router-dom";
+import SearchResultInfo from "../components/SearchResultInfo";
+import SearchResultCard from "@/components/SearchResultCard";
+import { useState } from "react";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import PaginationSelector from "@/components/PaginationSelector";
+import CuisineFilter from "@/components/CuisinesFilter";
+import SortOptionDropDown from "@/components/SortOptionDropDown";
+import { Loader2 } from "lucide-react";
+
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+  sortOption: string;
+};
+
+const SearchPage = () => {
+  const { city } = useParams();
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+    page: 1,
+    selectedCuisines: [],
+    sortOption: "bestMatch",
+  });
+
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSortOption = (sortOption: string) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      sortOption,
+      page: 1,
+    }));
+  };
+
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      selectedCuisines,
+      page: 1,
+    }));
+  };
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      page,
+    }));
+  };
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+      page: 1,
+    }));
+  };
+
+  // //debugging
+  // const setSearchQuery = (searchFormData: SearchForm) => {
+  //   setSearchState((prevState) => ({
+  //     ...prevState,
+  //     searchQuery: searchFormData.searchQuery || "",
+  //   }));
+  // };
+  // //
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+      page: 1,
+    }));
+  };
+
+  // if (isLoading) {
+  //   <span>Loading...</span>;
+  // }
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-40">
+        <Loader2 className="h-10 w-10 animate-spin text-gray-500" />
+      </div>
+    );
+  }
+  if (!results?.data || !city) {
+    return <span>No results found!</span>;
+  }
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5 px-4 md:px-8 lg:px-0">
+      <div id="cuisines-list">
+        <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() =>
+            setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+          }
+        />
+      </div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder="Search by Cuisines or Restaurant Name"
+          onReset={resetSearch}
+        />
+        <div className="flex justify-between flex-col gap-3 lg:flex-row">
+          <SearchResultInfo total={results.pagination.total} city={city} />
+          <SortOptionDropDown
+            sortOption={searchState.sortOption}
+            onChange={(value) => setSortOption(value)}
+          />
+        </div>
+        {results.data.map((restaurant) => (
+          <SearchResultCard
+            // key={restaurant.id}
+            restaurant={restaurant}
+          />
+        ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
+      </div>
+    </div>
+  );
+};
+
+// const SearchPage = () => {
+//   const { city } = useParams();
+//   const navigate = useNavigate();
+//   const [SearchState, setSearchState] = useState<SearchState>({
+//     searchQuery: "",
+//   });
+//   const { results, isLoading } = useSearchRestaurants(SearchState, city);
+
+//   const setSearchQuery = (searchFormData: SearchForm) => {
+//     setSearchState((prevState) => ({
+//       ...prevState,
+//       searchQuery: searchFormData.searchQuery, // Guaranteed to be a string
+//     }));
+//     navigate(`/search/${searchFormData.searchQuery}`);
+//   };
+
+//   const resetSearch = () => {
+//     setSearchState((prevState) => ({
+//       ...prevState,
+//       searchQuery: "",
+//     }));
+//     navigate("/search");
+//   };
+
+//   if (isLoading) {
+//     return <span>Loading...</span>;
+//   }
+//   if (!results?.data || !city) {
+//     return <span>No results found!</span>;
+//   }
+
+//   return (
+//     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+//       <div id="cuisines-list">insert cuisines here</div>
+//       <div id="main-content" className="flex flex-col gap-5">
+//         <SearchBar
+//           searchQuery={SearchState.searchQuery}
+//           onSubmit={setSearchQuery}
+//           placeHolder="Search by Cuisines or Restaurant Name"
+//           onReset={resetSearch}
+//         />
+//         {results.data.length > 0 ? (
+//           <>
+//             <SearchResultInfo total={results.pagination.total} city={city} />
+//             {results.data.map((restaurant) => (
+//               <SearchResultCard restaurant={restaurant} />
+//             ))}
+//           </>
+//         ) : (
+//           <span>No restaurants found for "{city}"</span>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+export default SearchPage;
